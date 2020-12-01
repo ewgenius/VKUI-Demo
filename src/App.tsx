9b1838b461da95259b727864edc5490b051526d0
Evgenii Khramkov <ewgeniux@gmail.com>
2020-12-01 15:32:03 +0300

    switch to full app VKUI integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import {
-  AppRoot,
   View,
   Panel,
   PanelHeader,
@@ -22,25 +21,23 @@ export function App() {
       webviewType={WebviewType.INTERNAL}
       isWebView
     >
-      <AdaptivityProvider embedded>
-        <AppRoot>
-          <SplitCol width="100%" animate={false}>
-            <View activePanel={panel}>
-              <Panel id="1">
-                <PanelHeader left={<PanelHeaderBack />}>Test app</PanelHeader>
-                <Group>
-                  <CellButton onClick={() => setPanel("2")}>Panel 2</CellButton>
-                </Group>
-              </Panel>
-              <Panel id="2">
-                <PanelHeader left={<PanelHeaderBack />}>Test app</PanelHeader>
-                <Group>
-                  <CellButton onClick={() => setPanel("1")}>Panel 1</CellButton>
-                </Group>
-              </Panel>
-            </View>
-          </SplitCol>
-        </AppRoot>
+      <AdaptivityProvider>
+        <SplitCol width="100%" animate>
+          <View activePanel={panel}>
+            <Panel id="1">
+              <PanelHeader left={<PanelHeaderBack />}>Test app</PanelHeader>
+              <Group>
+                <CellButton onClick={() => setPanel("2")}>Panel 2</CellButton>
+              </Group>
+            </Panel>
+            <Panel id="2">
+              <PanelHeader left={<PanelHeaderBack />}>Test app</PanelHeader>
+              <Group>
+                <CellButton onClick={() => setPanel("1")}>Panel 1</CellButton>
+              </Group>
+            </Panel>
+          </View>
+        </SplitCol>
       </AdaptivityProvider>
     </ConfigProvider>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,4 @@ import ReactDOM from "react-dom";
 import "@vkontakte/vkui/dist/vkui.css";
 import { App } from "./App";
 
-const Root = () => (
-  <div style={{ display: "flex", flexFlow: "column", alignItems: "center" }}>
-    <h1>Embedded VKUI App</h1>
-    <div>
-      <input placeholder="some native form elemnts" />
-      <br />
-      <textarea />
-    </div>
-    <div
-      style={{
-        marginTop: 20,
-        minWidth: 500,
-        maxWidth: 800,
-        border: "1px solid black",
-      }}
-    >
-      <App />
-    </div>
-  </div>
-);
-
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
